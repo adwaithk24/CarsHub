4688b37e9b8153caa74ec74ffcd139b756252c73
ShreemoyN97 <nanda.shr@northeastern.edu>
2023-04-14 22:18:43 -0400

    Added Procedure and Sequence

--- procedure/SP_VEHICLE_ONBOARDING.sql
+++ procedure/SP_VEHICLE_ONBOARDING.sql
@@ -1,5 +1,4 @@
 create or replace procedure sp_vehicle_onboarding(
-  --pi_vehicle_id VEHICLES.vehicle_id%type , 
   pi_vehicle_type VEHICLES.vehicle_type%type, 
   pi_vehicle_manufacturer VEHICLES.vehicle_manufacturer%type , 
   pi_vehicle_model VEHICLES.vehicle_model%type , 
@@ -10,27 +9,37 @@ create or replace procedure sp_vehicle_onboarding(
   pi_vehicle_owner VEHICLES.vehicle_owner%type, 
   pi_base_cost VEHICLES.base_cost%type, 
   pi_purchase_date VEHICLES.purchase_date%type, 
-  --pi_vehicle_status VEHICLES.vehicle_status%type, 
   pi_store_id VEHICLES.store_id%type 
 ) as 
 v_vehicle_count number;
 e_vehicle_exists exception;
 begin
-    select count(*) into v_vehicle_count from vehicles where chasis_no=pi_chasis_no;
-    if v_vehicle_count > 0 then
+    select count(*) into v_vehicle_count from vehicles where chasis_no=pi_chasis_no or pi_vehicle_no=vehicle_no or pi_engine_no=engine_no;
+    --if lower(pi_vehicle_type) != 'sedan' or lower(pi_vehicle_type) != 'hactchback' or lower(pi_vehicle_type) != 'convertible' or lower(pi_vehicle_type) != 'coupe' or lower(pi_vehicle_type) != 'suv' then
+    --   dbms_output.put_line('----------Invalid Vehicle Type----------');
+    --els
+    if length(pi_vehicle_no)>20 --or REGEXP_LIKE(pi_vehicle_no, '^[[:alnum:][:space:]\-\'']+$') 
+    then
+        dbms_output.put_line('----------Invalid Vehicle Number----------');
+    elsif length(pi_chasis_no)<=12 then
+        dbms_output.put_line('----------Invalid Chasis Number----------');
+    elsif length(pi_engine_no)!=8 then
+        dbms_output.put_line('----------Invalid Engine Number----------');
+    elsif v_vehicle_count > 0 then
         raise e_vehicle_exists;
     else
         insert into VEHICLES(vehicle_id,vehicle_type, 
- vehicle_manufacturer, vehicle_model, vehicle_no, chasis_no, 
-engine_no, vehicle_location, vehicle_owner, base_cost,purchase_date, store_id ) 
-values 
-            (s_vehicle_id.NEXTVAL, pi_vehicle_type, pi_vehicle_manufacturer, pi_vehicle_model, pi_vehicle_no, pi_chasis_no, pi_engine_no, 
-            pi_vehicle_location, pi_vehicle_owner, pi_base_cost, pi_purchase_date, pi_store_id);
-        commit;
+                    vehicle_manufacturer, vehicle_model, vehicle_no, chasis_no, 
+                    engine_no, vehicle_location, vehicle_owner, base_cost,purchase_date, store_id ) 
+                    values 
+                    (s_vehicle_id.NEXTVAL, pi_vehicle_type, pi_vehicle_manufacturer, pi_vehicle_model, pi_vehicle_no, pi_chasis_no, pi_engine_no, 
+                    pi_vehicle_location, pi_vehicle_owner, pi_base_cost, pi_purchase_date, pi_store_id);
+        commit; 
         dbms_output.put_line('vehicle added');        
     end if;
 exception 
     when e_vehicle_exists then
        dbms_output.put_line('vehicle already exists!');
-
+    when others then
+           dbms_output.put_line('entry invalid!');
 end sp_vehicle_onboarding;
--- procedure/sp_employee_onboarding.sql
+++ procedure/sp_employee_onboarding.sql
@@ -15,7 +15,9 @@ create or replace procedure sp_employee_onboarding(
     pi_salary employees.salary%type,
     pi_date_of_joining employees.date_of_joining%type
 )as 
-    v_employee_count number(1); 
+    v_employee_count number(1);
+    v_email_id number;
+    v_phone_no number;
     v_manager_id number;
     v_manager_count number(1);
     v_emp_store number;
@@ -24,38 +26,45 @@ create or replace procedure sp_employee_onboarding(
     e_designation_not_exists exception;
     e_manager_exists exception;
     e_store_does_not_exist exception;
+    e_phone_no exception;
+    e_email_id exception;
+    e_ssn_no exception;
 begin
-    select count(*) into v_employee_count from employees where ssn_no = pi_ssn_no; -- Counter for checking the unique dataset for employees table based on unique SSN Number
+    select count(*) into v_employee_count from employees where ssn_no = pi_ssn_no or email_id=pi_email_id or phone_no=pi_phone_no; -- Counter for checking the unique dataset for employees table based on unique SSN Number
     select count(*) into v_manager_count from employees where store_id = pi_store_id and lower(designation) = 'manager'; -- Counter for checking there is no 2 managers assigned to the same store
     select count(*) into v_store_exists from store_location where store_id = pi_store_id; -- Counter for checking there is no data entered in employees table for non existent store
-    if v_store_exists = 0 then
+    if pi_ssn_no not BETWEEN 100000001 AND 999999999 then
+        raise e_ssn_no;
+    elsif pi_email_id not like('%@%.%') then
+        raise e_email_id;
+    elsif pi_phone_no not BETWEEN 1111111111 and 9999999999 then
+        raise e_phone_no;
+    elsif v_store_exists = 0 then
         raise e_store_does_not_exist;
-    else if v_employee_count > 0 then
+    elsif v_employee_count > 0 then
         RAISE e_employee_exists;
-
-    else if lower(pi_designation) = 'manager' and v_manager_count = 0 then
+    elsif lower(pi_designation) = 'manager' and v_manager_count = 0 then
         insert into employees values(s_employees_id.nextval,pi_first_name,pi_last_name,pi_ssn_no,pi_designation,pi_phone_no,pi_email_id,pi_store_id,pi_address_line_1,pi_address_line_2,pi_city,pi_state,pi_country,pi_zip_code,1,pi_salary,pi_date_of_joining);
-
-    else if lower(pi_designation) = 'manager' and v_manager_count > 0 then
+    elsif lower(pi_designation) = 'manager' and v_manager_count > 0 then
         raise e_manager_exists;
-
-    else if lower(pi_designation) = 'employee' then
+    elsif lower(pi_designation) = 'employee' then
         select employee_id into v_manager_id from employees where store_id=pi_store_id and lower(designation) = 'manager';
         insert into employees values(s_employees_id.nextval,pi_first_name,pi_last_name,pi_ssn_no,pi_designation,pi_phone_no,pi_email_id,pi_store_id,pi_address_line_1,pi_address_line_2,pi_city,pi_state,pi_country,pi_zip_code,v_manager_id,pi_salary,pi_date_of_joining);
         dbms_output.put_line('----------------------------Employee created----------------------------');
-
     else
         RAISE e_designation_not_exists;
         --DBMS_OUTPUT.PUT_LINE('--------------------------Designation does not exists--------------------------');
 
     end if;
-    end if;
-    end if;
-    end if;
-    end if;
     commit;
 
 EXCEPTION
+    WHEN e_ssn_no THEN
+        DBMS_OUTPUT.PUT_LINE('------------------SSN Number not in required length------------------');
+    WHEN e_email_id THEN
+        DBMS_OUTPUT.PUT_LINE('------------------Email is in wrong format------------------');
+    WHEN e_phone_no THEN
+        DBMS_OUTPUT.PUT_LINE('------------------Phone number is in wrong format------------------');
     WHEN e_store_does_not_exist THEN
         DBMS_OUTPUT.PUT_LINE('-----------------------------Store does not exists-----------------------------');
     WHEN e_employee_exists THEN
